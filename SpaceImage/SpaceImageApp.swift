4b4532953fa9061bee57fc28c4009e9ffec47f59
ODM <danmusad@gmail.com>
2020-10-07 14:54:22 -0500

    Initial Commit

---
+++ SpaceImage/SpaceImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceImageApp.swift
+//  SpaceImage
+//
+//  Created by ODM on 10/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SpaceImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
